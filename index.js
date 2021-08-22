c8d160f812eb61314571dc54dd9c33025f56c981
Sahstiva <avitshas@gmail.com>
2021-08-22 22:36:09 +0300

    add table, lang, antity

--- index.js
+++ index.js
@@ -6,10 +6,128 @@ const connection = mysql.createConnection({
     user:   'bondibon',
     database:   'bondibon',
     password:   'bondibon',
-    port:       8889
+    multipleStatements: true
+    //port:       8889
 });
 
-const pages = readLinks();
+let fields = [
+                { name: 'UF_TITLE', type: 'text' },
+                { name: 'UF_PAGEID', type: 'text' }
+             ];
+let hlBlock = {
+    name: 'Testpages',
+    language:   [
+                    { lid: 'ru', name: 'Тестовый блок'},
+                    {lid: 'en', name: 'Test block'}
+                ],
+    table: 'bb_test_pages3',
+    fields : [
+                { name: 'UF_TITLE', type: 'text' },
+                { name: 'UF_PAGEID', type: 'text' }
+            ]
+};
+createHlblock(hlBlock);
+// const pages = readLinks();
+
+/* 1. Проверка, есть таблица в БД или нет */
+async function createHlblock(hlBlock) {
+    connection.promise().query('SELECT * FROM INFORMATION_SCHEMA.TABLES WHERE TABLE_NAME = ?',[hlBlock.table])
+        .then( ([rows, fields]) => {
+            if(rows.length > 0) {
+                console.log(`Таблица ${ hlBlock.table } существует в БД`);
+                connection.end();
+            }
+            else {
+                console.log(`Таблица ${hlBlock.table} в базе не найдена`);
+                createHlblockTable(hlBlock);
+            }
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+/* 2. Создание таблицы в БД */
+async function createHlblockTable(hlBlock) {
+    connection.promise().query(`CREATE TABLE ${hlBlock.table} ( ID int NOT NULL AUTO_INCREMENT, PRIMARY KEY (ID));`)
+        .then(() => {
+            let sqlQuery = `ALTER TABLE ${hlBlock.table} `;
+            hlBlock.fields.forEach(field => {
+                sqlQuery += `ADD COLUMN ${field.name} ${field.type},`;
+            });
+            connection.promise().query(sqlQuery.slice(0,-1))
+                .then(() => {
+                    console.log(`Таблица ${hlBlock.table} создана`);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
+            addHlblockEntity(hlBlock);
+        })
+        .catch(err => {
+            console.log(err);
+            connection.end();
+        })
+}
+
+/* 3. Добавление новой записи в список Hightload-блоков */
+async function addHlblockEntity(hlBlock) {
+    connection.promise().query(`INSERT INTO b_hlblock_entity (NAME, TABLE_NAME) VALUES ('${hlBlock.name}','${hlBlock.table}' )`)
+        .then(([result,fields]) => {
+            console.log(`Запись для таблицы ${hlBlock.table} для highloadblock-а ${hlBlock.name} создана. ID - ${result.insertId}`);
+            hlBlock.id = result.insertId;
+            addHlblockLang(hlBlock);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+/* 4. Добавление названий на русском и английском языках для HighLoad-блока  */
+async function addHlblockLang(hlBlock) {
+    let sqlQuery = 'INSERT INTO b_hlblock_entity_lang (ID, LID, NAME)' + ' VALUES';
+    hlBlock.language.forEach(lang => {
+        sqlQuery += `('${hlBlock.id}', '${lang.lid}', '${lang.name}'),`;
+    })
+    connection.promise().query(sqlQuery.slice(0,-1))
+        .then(([result,fields]) => {
+            console.log(`Добавлены ${result.affectedRows} названий для highloadblock-а ${hlBlock.name}`)
+            addHlblockUserFields(hlBlock);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+}
+
+/* 5. Добавление записей в таблицу пользовательских типов  */
+async function addHlblockUserFields(hlBlock) {
+    //
+    // ID
+    // ENTITY_ID
+    // FIELD_NAME
+    // USER_TYPE_ID
+    // XML_ID
+    // SORT
+    // MULTIPLE
+    // MANDATORY
+    // SHOW_FILTER
+    // SHOW_IN_LIST
+    // EDIT_IN_LIST
+    // IS_SEARCHABLE
+    // SETTINGS
+
+    // HLBLOCK_7
+    // UF_SHOP
+    // hlblock
+    // 100
+    // N
+    // N
+    // N
+    // Y
+    // Y
+    // N
+    // a:5:{s:7:"DISPLAY";s:4:"LIST";s:11:"LIST_HEIGHT";i:5;s:10:"HLBLOCK_ID";i:9;s:10:"HLFIELD_ID";i:106;s:13:"DEFAULT_VALUE";i:0;}
+    // a:5:{s:7:"DISPLAY";s:4:"LIST";s:11:"LIST_HEIGHT";i:5;s:10:"HLBLOCK_ID";i:10;s:10:"HLFIELD_ID";i:109;s:13:"DEFAULT_VALUE";i:0;}
+}
+
 
 async function readLinks() {
     fs.readFile('links.json', 'utf8',(error, data) => {
